4ecf930735299d119e1eae7363662e18a6b4dd16
elliot richardson <ear78@aol.com>
2017-06-16 15:21:55 -0500

    added page loader basics

--- js/app.module.js
+++ js/app.module.js
@@ -1,1 +1,10 @@
 var app = angular.module('todo', []);
+
+app.run(function($rootScope, $timeout){
+        $rootScope.isShowing = true;
+
+        $timeout(function(){
+            $rootScope.isShowing = false;
+        }, 3000);
+
+});
