d60291f3c59f9bfa6a93ed351b1f1c71e4e2c5ad
elliot richardson <ear78@aol.com>
2017-06-17 00:38:13 -0500

    added count of list to controller, restyled list

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,6 +2,7 @@ app.controller('mainCtrl', function($scope){
 
     $scope.isError = true;
     $scope.products = ["Milk", "Bread", "Cheese"];
+    $scope.count = $scope.products.length;// count for list
 
     // ADD ITEM TO TODO LIST
     $scope.addItem = function() {
@@ -16,6 +17,7 @@ app.controller('mainCtrl', function($scope){
             $scope.products.push($scope.addMe);
             $scope.addMe = "";
             $scope.isError = true;
+            $scope.count++;
         }
         else {
             $scope.errorText = "The item you added is already on the list.";
@@ -26,8 +28,10 @@ app.controller('mainCtrl', function($scope){
     // REMOVE ITEM FROM TODO LIST
     $scope.removeItem = function(product){
         $scope.errorText = "";
-
         $scope.products.splice(product, 1);
+        $scope.count--;
     }
+
+
 
 })
